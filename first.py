7e2323d8626f271bad023095d80112ad545c1f6a
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-10-03 08:57:37 +0000

    first

--- first.py
+++ first.py
@@ -1,5 +1,6 @@
 print("Hello World!")
 def sudy_lichy(cislo):
-    print('Cislo', cislo, "je sude")
+    print('Cislo', cislo // 2, "je sude")
+    print('Cislo', cislo !// 2, "je lichy")
 sudy_lichy(5)
 sudy_lichy(1000000)
