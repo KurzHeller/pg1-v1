1f5e47758fe28df41cf103175c6af831be8c4fb9
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-10-03 08:04:48 +0000

    prvni program

---
+++ first.py
@@ -0,0 +1,5 @@
+print("Hello World!")
+#def sudy_lichy(cislo):
+    pass 
+sudy_lichy(5)
+sudy_lichy(1000000)
---
+++ fivestars.py
@@ -0,0 +1,7 @@
+print("zacatek")
+i = 0
+
+while  i<5:
+    print("*")
+i += 1
+print("Konec")
