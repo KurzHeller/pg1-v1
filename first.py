631b578eef2fe110de789356d9bdeb42654c05ab
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-10-03 09:14:33 +0000

    hot

--- first.py
+++ first.py
@@ -1,10 +1,10 @@
 
-print("Hello World!")
+#print("Hello World!")
 def sudy_lichy(cislo):
-    if cislo % 2 == 0
-    print('Cislo', cislo, "je sude")
-    else 
-    print('Cislo', cislo, "je lichy")
-#sudy_lichy(5)
-#sudy_lichy(1000000)
+    if cislo % 2 == 0:
+     print('Cislo', cislo, "je sude"), 
+    else:
+     print('Cislo', cislo, "je lichy")
+sudy_lichy(5)
+sudy_lichy(1000000)
 
