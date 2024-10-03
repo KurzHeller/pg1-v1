cd1ae2e126aedc4ed5837531a82d6381aeeb17d0
HryhoriiZhaliy <zhalih00@jcu.cz>
2024-10-03 08:48:56 +0000

    dalsi

--- fivestars.py
+++ fivestars.py
@@ -1,4 +1,6 @@
-def in_range(min_number, max_number, number):
-        print('Is in range')
-        print('Is not in range')
-in_range(100, 1000, 1)
+# funkce
+def zobraz_pozdrav(jmeno):
+    print('Ahoj', jmeno)
+
+jm = input("Zadej_jmeno:")
+zobraz_pozdrav(jm)
