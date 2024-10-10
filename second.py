fcc2a1cebfd00ab9cdfac7c34a8fc5068b0c04ff
KurzHeller <139373163+KurzHeller@users.noreply.github.com>
2024-10-10 20:18:47 +0300

    Update second.py

--- second.py
+++ second.py
@@ -1,4 +1,3 @@
-# Funkce pro převod čísel na jejich textovou reprezentaci
 def cislo_text(cislo):
     cisla = {
         0: "nula", 1: "jedna", 2: "dva", 3: "tři", 4: "čtyři",
@@ -6,11 +5,8 @@ def cislo_text(cislo):
         10: "deset", 11: "jedenáct", 12: "dvanáct", 13: "třináct", 14: "čtrnáct",
         15: "patnáct", 16: "šestnáct", 17: "sedmnáct", 18: "osmnáct", 19: "devatenáct",
         20: "dvacet", 30: "třicet", 40: "čtyřicet", 50: "padesát",
-        60: "šedesát", 70: "sedmdesát", 80: "osmdesát", 90: "devadesát", 100: "sto"
-    }
-
+        60: "šedesát", 70: "sedmdesát", 80: "osmdesát", 90: "devadesát", 100: "sto"}
     cislo = int(cislo)
-
     if cislo in cisla:
         return cisla[cislo]
     elif cislo < 100:
@@ -19,8 +15,8 @@ def cislo_text(cislo):
         return f"{cisla[desitky]} {cisla[jednotky]}"
     else:
         return "Číslo je mimo rozsah"
+    
 
-# Hlavní část programu
 if name == "main":
     cislo = input("Zadej číslo: ")
     text = cislo_text(cislo)
